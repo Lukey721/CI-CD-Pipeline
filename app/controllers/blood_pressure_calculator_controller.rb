f17f7ce1d8035beb6fb0a8cdd6b948af62f402e9
Lukey721 <lukeyconnolly@hotmail.com>
2024-10-19 22:14:43 +0100

    Update to add validation

--- app/controllers/blood_pressure_calculator_controller.rb
+++ app/controllers/blood_pressure_calculator_controller.rb
@@ -1,26 +1,37 @@
 class BloodPressureCalculatorController < ApplicationController
-  
+  SYSTOLIC_MIN = 70
+  SYSTOLIC_MAX = 190
+  DIASTOLIC_MIN = 40
+  DIASTOLIC_MAX = 100
+
   def new
+    # Only process form data if the request is POST
     if request.post?
       systolic = params[:systolic].to_i
       diastolic = params[:diastolic].to_i
 
-      # Call the categorize method to determine what is the blood pressure status 
-      @category = categorize_blood_pressure(systolic, diastolic)
+      if valid_blood_pressure?(systolic, diastolic)
+        # Valid input, show blood pressure category
+        @category = categorize_blood_pressure(systolic, diastolic)
+      else
+        # Invalid input, set error message
+        @error_message = "Please review the measurements you have entered, The Valid Values are: Systolic (#{SYSTOLIC_MIN}-#{SYSTOLIC_MAX}), Diastolic (#{DIASTOLIC_MIN}-#{DIASTOLIC_MAX})."
+      end
     end
   end
 
   private
 
-  #add method for validation of user answer TODO
+  def valid_blood_pressure?(systolic, diastolic)
+    systolic.between?(SYSTOLIC_MIN, SYSTOLIC_MAX) && diastolic.between?(DIASTOLIC_MIN, DIASTOLIC_MAX)
+  end
 
   def categorize_blood_pressure(systolic, diastolic)
-    case
-    when systolic < 90 || diastolic < 60
+    if systolic < 90 || diastolic < 60
       "Low blood pressure"
-    when systolic.between?(90, 120) && diastolic.between?(60, 80)
+    elsif systolic.between?(90, 120) && diastolic.between?(60, 80)
       "Ideal blood pressure"
-    when systolic.between?(120, 139) || diastolic.between?(80, 89)
+    elsif systolic.between?(120, 139) || diastolic.between?(80, 89)
       "Pre-high blood pressure"
     else
       "High blood pressure"
