1890820db5c30cde385ad36a4fa1969cc3bb443b
Lukey721 <lukeyconnolly@hotmail.com>
2024-11-21 22:43:52 +0000

    Additional BDD tests

--- features/Blood_pressure_answers.feature
+++ features/Blood_pressure_answers.feature
@@ -14,3 +14,15 @@ Feature: Blood Pressure Calculator
     When I fill in the blood pressure form with invalid details
     And I click the "Calculate" button
     Then I should see "Please review the measurements you have entered"
+
+  Scenario: Successful Pre-high blood pressure calculation
+    Given I am on the calculator page
+    When I fill in the blood pressure form with valid details to calculate pre-high blood pressure
+    And I click the "Calculate" button
+    Then I should see "Pre-high blood pressure"
+
+  Scenario: Successful high blood pressure calculation
+    Given I am on the calculator page
+    When I fill in the blood pressure form with valid details to calculate high blood pressure
+    And I click the "Calculate" button
+    Then I should see "High blood pressure"
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,14 +2,28 @@ Given('I am on the calculator page') do
   visit root_path
 end
 
+#ideal blood pressure BDD test
 When('I fill in the blood pressure form with valid details') do
   fill_in 'systolic', with: '120'
   fill_in 'diastolic', with: '80'
 end
 
+#Invalid BDD test
 When('I fill in the blood pressure form with invalid details') do
   fill_in 'systolic', with: '2000'  # Invalid input
   fill_in 'diastolic', with: '3'
+end
+
+#pre-high blood pressure BDD test
+When('I fill in the blood pressure form with valid details to calculate pre-high blood pressure') do
+  fill_in 'systolic', with: '150' 
+  fill_in 'diastolic', with: '85'
+end
+
+#high blood pressure BDD test
+When('I fill in the blood pressure form with valid details to calculate high blood pressure') do
+  fill_in 'systolic', with: '141' 
+  fill_in 'diastolic', with: '91'
 end
 
 When('I click the {string} button') do |button|
@@ -17,5 +31,5 @@ When('I click the {string} button') do |button|
 end
 
 Then('I should see {string}') do |message|
-  expect(page).to have_content("Ideal blood pressure")
+  expect(page).to have_content(message)
 end
