c58bccf51f0ae8170e5ec76e96c5bc393b0748a7
Lukey721 <lukeyconnolly@hotmail.com>
2024-11-27 18:56:56 +0000

    remove secure headers

--- config/initializers/secure_headers.rb
+++
@@ -1,7 +0,0 @@
-SecureHeaders::Configuration.default do |config|
-  config.csp = {
-    default_src: ["'self'"],# Allows content only from the same origin
-    object_src: ["'none'"], # Disallow plugins
-    frame_ancestors: ["'none'"] # Disallow embedding
-  }
-end
