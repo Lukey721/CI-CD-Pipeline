41bafc603bdc0dc3673453ca2079a7c306a97d75
Lukey721 <lukeyconnolly@hotmail.com>
2024-12-03 17:17:52 +0000

    application_insights test

--- config/initializers/application_insights.rb
+++ config/initializers/application_insights.rb
@@ -1,7 +1,5 @@
 require 'application_insights'
 
-ApplicationInsights.setup do |config|
-  config.instrumentation_key = ENV.fetch('3e1c8387-ebe6-432f-bdf3-af4ef9658d98', nil)
-end
-
-Rails.logger.info('Application Insights has been configured.')
+ApplicationInsights::TelemetryClient.new(
+  ENV.fetch('3e1c8387-ebe6-432f-bdf3-af4ef9658d98', nil)
+)
